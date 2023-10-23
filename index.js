187a82f0540f4539618ed1d008e3bd11a077aace
Maxwell <njoromax5@gmail.com>
2023-10-23 16:06:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    changeText();
+})
+function changeText(){
+    let paragraph = document.querySelector("p");
+    paragraph.textContent = "This is really cool!"
+}
